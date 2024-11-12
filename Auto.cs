6af5fadad445dd72d56f05b9b5bde7e55f4a6761
SysifosWeb <osman_ahumada@hotmail.com>
2024-11-12 01:21:06 -0300

    propiedades de auto

---
+++ Auto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Auto
+    {
+        public string Matricula { get; set; }
+        public string Marca { get; set; }
+        public string Tipo { get; set; }
+        public string Color { get; set; }
+
+        public Auto(string matricula, string marca, string tipo, string color) {        
+            Matricula = matricula;
+            Marca = marca;
+            Tipo = tipo;
+            Color = color;
+        }
+
+        public void Avanzar() {
+            Console.WriteLine("Mi {0} esta avanzando...",Marca);
+        }
+
+        public void Frenar()
+        {
+            Console.WriteLine("Mi {0} esta frenando...", Marca);
+        }
+
+        public void Estacionar()
+        {
+            Console.WriteLine("Estacione mi {0}...", Marca);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ConsoleApp1;
+
+Auto Auto_1 = new Auto("B4F945","Ferrari","Coupe","Celeste");
+
+Console.WriteLine("**********************************");
+Console.WriteLine("****      Mi Clase Auto        ***");
+Console.WriteLine("**********************************");
+Console.WriteLine("*                                *");
+Console.WriteLine("* Matricula    : {0}          *",Auto_1.Matricula);
+Console.WriteLine("* Marca        : {0}         *",Auto_1.Marca);
+Console.WriteLine("* Tipo de auto : {0}           *",Auto_1.Tipo);
+Console.WriteLine("* Color        : {0}         *",Auto_1.Color);
+Console.WriteLine("*                                *");
+Console.WriteLine("**********************************");
+
